ccb3b0f3104841962e9c52ed42a9004874c41b48
杨耀飞 <yangyaofei@gmail.com>
2022-05-06 15:47:54 +0800

    finish test

--- src/test/java/com/lingjoin/elasticsearch/index/IctclasAnalysisFactoryTests.java
+++ src/test/java/com/lingjoin/elasticsearch/index/IctclasAnalysisFactoryTests.java
@@ -1,6 +1,9 @@
 package com.lingjoin.elasticsearch.index;
 
 
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.indices.analysis.AnalysisFactoryTestCase;
 
 import java.util.HashMap;
@@ -12,15 +15,24 @@ public class IctclasAnalysisFactoryTests extends AnalysisFactoryTestCase {
         super(new IctclasAnalysisPlugin());
     }
 
-    @Override
-    protected Map<String, Class<?>> getTokenizers() {
-        Map<String, Class<?>> tokenizers = new HashMap<>(super.getTokenizers());
-        tokenizers.put("ictclas_index", IctclasAnalysisPlugin.class);
-        return tokenizers;
+    static class IctclasIndexTokenizerFactoryTest extends IctclasTokenizerFactory{
+
+        public IctclasIndexTokenizerFactoryTest(IndexSettings indexSettings, Environment environment, Settings settings, String name) {
+            super(indexSettings, environment, settings, name, true);
+        }
+    }
+    static class IctclasSearchTokenizerFactoryTest extends IctclasTokenizerFactory{
+        public IctclasSearchTokenizerFactoryTest(IndexSettings indexSettings, Environment environment, Settings settings, String name) {
+            super(indexSettings, environment, settings, name, false);
+        }
     }
 
     @Override
-    public void testTokenizers() {
-        super.testTokenizers();
+    protected Map<String, Class<?>> getTokenizers() {
+        Map<String, Class<?>> tokenizers = new HashMap<>(super.getTokenizers());
+        tokenizers.put("ictclas_index", IctclasIndexTokenizerFactoryTest.class);
+        tokenizers.put("ictclas_search", IctclasSearchTokenizerFactoryTest.class);
+        return tokenizers;
     }
+
 }
--- src/test/java/com/lingjoin/elasticsearch/index/IctclasAnalysisTests.java
+++ src/test/java/com/lingjoin/elasticsearch/index/IctclasAnalysisTests.java
@@ -8,6 +8,7 @@ import org.elasticsearch.Version;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
+import org.elasticsearch.index.Index;
 import org.elasticsearch.index.analysis.AnalysisTestsHelper;
 import org.elasticsearch.index.analysis.IndexAnalyzers;
 import org.elasticsearch.index.analysis.NamedAnalyzer;
@@ -16,33 +17,56 @@ import org.elasticsearch.test.ESTestCase;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.net.URL;
 import java.nio.file.Path;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.*;
 
 public class IctclasAnalysisTests extends ESTestCase {
     public void testDefaultsAnalysis() throws IOException {
+
         TestAnalysis analysis = createTestAnalysis();
 
-        TokenizerFactory tokenizerFactory = analysis.tokenizer.get("ictclas_index");
-        assertThat(tokenizerFactory, instanceOf(IctclasTokenizerFactory.class));
+        TokenizerFactory tokenizerIndexFactory = analysis.tokenizer.get("ictclas_index");
+        assertThat(tokenizerIndexFactory, instanceOf(IctclasTokenizerFactory.class));
+        TokenizerFactory tokenizerSearchFactory = analysis.tokenizer.get("ictclas_search");
+        assertThat(tokenizerSearchFactory, instanceOf(IctclasTokenizerFactory.class));
 
-        IndexAnalyzers indexAnalyzers = analysis.indexAnalyzers;
-        NamedAnalyzer analyzer = indexAnalyzers.get("ictclas_index");
-        assertThat(analyzer.analyzer(), instanceOf(IctclasAnalyzer.class));
 
-        assertThat(analyzer.analyzer().tokenStream(null, new StringReader("")), instanceOf(IctclasTokenizer.class));
+        IndexAnalyzers analyzers = analysis.indexAnalyzers;
+        NamedAnalyzer indexAnalyzer = analyzers.get("ictclas_index");
+        assertThat(indexAnalyzer.analyzer(), instanceOf(IctclasAnalyzer.class));
+        NamedAnalyzer searchAnalyzer = analyzers.get("ictclas_search");
+        assertThat(searchAnalyzer.analyzer(), instanceOf(IctclasAnalyzer.class));
 
+
+        assertThat(indexAnalyzer.analyzer().tokenStream(null, new StringReader("")), instanceOf(IctclasTokenizer.class));
+
+        String source = "一切有权力的人都容易滥用权力，这是一条千古不变的经验";
+        String[] expected = new String[]{"一切", "有", "权力", "的", "人", "都", "容易", "滥用", "权力", "，", "这", "是", "一", "条", "千古", "不", "变", "的", "经验"};
+
+        try (TokenStream indexTokenStream = indexAnalyzer.analyzer().tokenStream(null, new StringReader(source))) {
+            assertSimpleTSOutput(indexTokenStream, expected);
+        }
+
+        try (TokenStream searchTokenStream = searchAnalyzer.analyzer().tokenStream(null, new StringReader(source))) {
+            assertSimpleTSOutput(searchTokenStream, expected);
+        }
+
+        String sourceIr = "国务院办公厅转发商务部";
+        String[] expectedIndex = new String[]{"国务院办公厅", "国务院", "办公厅", "转发", "商务部"};
+        String[] expectedSearch = new String[]{"国务院办公厅", "转发", "商务部"};
+
+
+        try (TokenStream indexTokenStream = indexAnalyzer.analyzer().tokenStream(null, new StringReader(sourceIr))) {
+            assertSimpleTSOutput(indexTokenStream, expectedIndex);
+        }
+
+        try (TokenStream searchTokenStream = searchAnalyzer.analyzer().tokenStream(null, new StringReader(sourceIr))) {
+            assertSimpleTSOutput(searchTokenStream, expectedSearch);
+        }
     }
-
-//    public void testBaseFormFilterFactory() throws IOException, NlpirException {
-//        String source = "一切有权力的人都容易滥用权力，这是一条千古不变的经验";
-//        String[] expected = new String[]{"一切", "有", "权力", "的", "人", "都", "容易", "滥用", "权力", "，", "这", "是", "一", "条", "千古", "不", "变", "的", "经验"};
-//
-//        Tokenizer tokenizer = new IctclasTokenizer("packaging/", 1, "", null, false, false);
-//        tokenizer.setReader(new StringReader(source));
-//        assertSimpleTSOutput(tokenizer, expected);
-//    }
 
     public static void assertSimpleTSOutput(TokenStream stream, String[] expected) throws IOException {
         stream.reset();
@@ -57,8 +81,15 @@ public class IctclasAnalysisTests extends ESTestCase {
     }
 
     private TestAnalysis createTestAnalysis() throws IOException {
-        Path home = createTempDir();
-        Settings settings = Settings.builder().put(IndexMetadata.SETTING_VERSION_CREATED, Version.CURRENT).put(Environment.PATH_HOME_SETTING.getKey(), home).build();
+        Path home = Optional.ofNullable(IctclasAnalysisTests.class.getResource("/plugins"))
+                .map(URL::getPath)
+                .map(Path::of)
+                .map(Path::getParent)
+                .orElse(createTempDir());
+        Settings settings = Settings.builder()
+                .put(IndexMetadata.SETTING_VERSION_CREATED, Version.CURRENT)
+                .put(Environment.PATH_HOME_SETTING.getKey(), home)
+                .build();
         return AnalysisTestsHelper.createTestAnalysisFromSettings(settings, new IctclasAnalysisPlugin());
     }
 }
